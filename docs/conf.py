09efb757c430fd8872cdf9e18406d4d87e3776be
Brendan Hagan <bhaganksu@gmail.com>
2022-08-11 00:28:53 -0400

    misc: sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,21 @@
 project = 'MagicSetEditor2'
 
 version = '2.3.1'
+release = '2.3.1'
+
+html_logo = 'logo.png'
+
+html_theme_options = {
+    'collapse_navigation': False,
+    'style_external_links': True,
+    'prev_next_buttons_location': 'both',
+    'style_nav_header_background': '#aa2828',
+}
+
+# html_static_path = ['_static']
+# html_css_files = [
+#     'css/custom.css'
+# ]
+# html_js_files = [
+#     'js/custom.js'
+# ]
